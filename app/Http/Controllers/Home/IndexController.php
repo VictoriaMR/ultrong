30d034d2a0b93f6a1e3019e8733dbc7fbe96776e
LiaoMingRong <849376723@qq.com>
2020-07-14 19:18:35 +0800

    实例化控制器

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
-// use App\Services\ProductService;
 use frame\html;
 
 /**
@@ -14,18 +13,10 @@ class IndexController extends Controller
 {
 	protected $serivces = null;
 
-	public function __construct()
-    {
-    	// $this->serivces = $serivces;
-    	$this->assign('title', '广州市奥创三维科技有限公司-首页');
-    }
-
 	public function index()
 	{	
 		Html::addCss('index');
-		// $this->assign('title', '广州市奥创三维科技有限公司-首页');
-		// 
-		$this->assign('index_welcome', 'WELCOME TO ZONGHENG INTELLIGENCE 3D PRINTER TECHNOLOGY.CO.,LTD');
+
 		return view();
 	}
 }
