8736b2b8eb9ba323b02bb903d005385aed2d95bd
LiaoMingRong <849376723@qq.com>
2020-08-04 14:57:49 +0800

    debug

---
+++ app/Models/ArticleData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Base as BaseModel;
+
+/**
+ * 	背景颜色类
+ */
+class ArticleData extends BaseModel
+{ 
+	//表名
+    public $table = 'article_data';
+}
---
+++ app/Services/ArticleListService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Base as BaseService;
+use App\Models\Article;
+
+class ArticleListService extends BaseService
+{
+    public function getList()
+    {
+        
+    }
+}
