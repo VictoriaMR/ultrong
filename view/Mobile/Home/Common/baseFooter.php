7b8690b7fbb6d0c323be9ee1c656b4a9e62f8582
LiaoMingRong <849376723@qq.com>
2020-08-28 18:13:08 +0800

    ebug

--- view/Mobile/Home/Common/baseFooter.php
+++ view/Mobile/Home/Common/baseFooter.php
@@ -1,22 +1,16 @@
 <div id="footer" class="bg-f5">
 	<div class="footer-nav">
-		<?php $temp = [
-				'name' => dist('网站首页'),
-				'url' => url(''),
-			];
-			array_unshift($nav_list, $temp);
-			$tempData = array_chunk($nav_list, 4);
-		?>
-		<?php foreach ($tempData as $key => $value) { ?>
-			<ul>
-				<?php foreach ($value as $k => $v) { ?>
-					<li>
-						<a class="block color-white font-14" href="<?php echo $v['url'];?>"><?php echo $v['name'];?></a>
-					</li>
-				<?php } ?>
-			</ul>
-			<div class="clear"></div>
-		<?php }?>
+		<ul>
+			<li>
+				<a class="block color-white font-14" href="<?php echo url('');?>"><?php echo dist('首页');?></a>
+			</li>
+			<?php foreach ($nav_list as $k => $v) { ?>
+			<li>
+				<a class="block color-white font-14" href="<?php echo $v['url'];?>"><?php echo $v['name'];?></a>
+			</li>
+			<?php } ?>
+		</ul>
+		<div class="clear"></div>
 	</div>
 	<p class="margin-top-8 text-center">&copy;2020-<?php echo date('Y', strtotime('+1 year', strtotime(date('Y-m-d', time()))));?>&nbsp;<?php echo dist($site['name']);?>&reg;&nbsp;<?php echo dist('版权所有');?></p>
 	<?php if (!empty($site['ipc'])) { ?>
